db9e908926383631f2564401a3997d3415345836
Szymon Fiedler <szymon.fiedler@gmail.com>
2022-07-01 16:27:56 +0200

    Demostrate category select with onchange submit

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,32 +1,38 @@
 class TransactionsController < ApplicationController
   def index
     respond_to do |format|
-      format.html { render :index, locals: { transactions: transactions(params) } }
+      format.html { render :index, locals: { transactions: transactions(params), categories: categories } }
       format.turbo_stream do
         render turbo_stream.replace(
           "transactions",
           partial: "transactions/table",
-          locals: { transactions: transactions(params) })
+          locals:  { transactions: transactions(params) })
       end
     end
   end
 
   private
 
-  Transaction = Struct.new(:date, :title, :amount)
+  Transaction = Struct.new(:date, :title, :amount, :category)
   private_constant :Transaction
 
-  def transactions(params)
-    transactions = [
-      Transaction.new(Date.new(2022, 1, 1), "ABC", 123.45),
-      Transaction.new(Date.new(2022, 2, 1), "BCD", 123.45),
-      Transaction.new(Date.new(2022, 3, 1), "CDE", 123.45),
-      Transaction.new(Date.new(2022, 4, 1), "EFG", 123.45),
-      Transaction.new(Date.new(2022, 5, 1), "GHI", 123.45),
+  def transactions_list
+    [
+      Transaction.new(Date.new(2022, 1, 1), "ABC", 123.45, "Category 1"),
+      Transaction.new(Date.new(2022, 2, 1), "BCD", 123.45, "Category 2"),
+      Transaction.new(Date.new(2022, 3, 1), "CDE", 123.45, "Category 1"),
+      Transaction.new(Date.new(2022, 4, 1), "EFG", 123.45, "Category 2"),
+      Transaction.new(Date.new(2022, 5, 1), "GHI", 123.45, "Category 3"),
     ]
+  end
 
-    return transactions unless params[:search].present?
+  def transactions(params)
+    transactions_list
+      .select { |t| params[:search].present? ? t.title.downcase.include?(params[:search].downcase) : true }
+      .select { |t| params[:category].present? ? t.category == params[:category] : true }
+  end
 
-    transactions.select { |t| t.title.downcase.include?(params[:search].downcase) }
+  def categories
+    transactions_list.map(&:category).uniq
   end
 end
